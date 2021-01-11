53a6a871c5e2f96ff1d4037c6614c6f26d887454
Axel Forsch <axel.forsch@tutanota.com>
2021-01-11 06:57:37 +0100

    added default values for sprockets (the values of my racing bike)

--- src/redux/settings/reducer.ts
+++ src/redux/settings/reducer.ts
@@ -4,8 +4,8 @@ import { SETTINGS_ACTION_TYPES } from "./actions";
 export const initialState: SettingsState = {
   frontSprocketNumber: 0,
   rearSprocketNumber: 0,
-  frontSprockets: [],
-  rearSprockets: [],
+  frontSprockets: [52,39,30],
+  rearSprockets: [13,14,15,16,17,18,19,21,23,25],
   favoriteCadence: 0,
 };
 
