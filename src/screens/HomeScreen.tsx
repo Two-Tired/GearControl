ebc55610277bd6d0756de3d6c87384cc01d1e5ac
Axel Forsch <axel.forsch@tutanota.com>
2021-01-11 17:09:10 +0100

    changed layout of HomeScreen (only for testing), removed package-lock.json

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import { Subheading, Divider, Text } from "react-native-paper";
+import { Subheading, Divider, Text, DataTable } from "react-native-paper";
 import { useSelector } from "react-redux";
 import { HomeScreenNavigationProp, HomeScreenRouteProp, SettingsState, AppState } from "../types";
+// import { MapView } from 'react-native-maps';
 
 type Props = {
   route: HomeScreenRouteProp;
@@ -14,8 +15,7 @@ export function HomeScreen({ route, navigation }: Props) {
 
   return (
     <View style={styles.container}>
-      
-      <Subheading>Front Sprockets</Subheading>
+      {/* <Subheading>Front Sprockets</Subheading> 
       {
         settings.frontSprockets.map((value, index) => {
           return <Text>{"Sprocket " + (index + 1) + ": " + value}</Text>
@@ -26,7 +26,32 @@ export function HomeScreen({ route, navigation }: Props) {
         settings.rearSprockets.map((value, index) => {
           return <Text>{"Sprocket " + (index + 1) + ": " + value}</Text>
         })
-      }
+      } */}
+      {/* <MapView style={styles.map} /> */}
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title>Front \ Rear</DataTable.Title>
+          {
+            settings.rearSprockets.map((value, index) => {
+              return <DataTable.Title numeric>{value}</DataTable.Title>
+            })
+          }
+        </DataTable.Header>
+        {
+          settings.frontSprockets.map((value, index) => {
+            return <DataTable.Row>
+              <DataTable.Cell >{value}</DataTable.Cell>
+              {
+                settings.rearSprockets.map((rearValue, rearIndex) => {
+                  return <DataTable.Cell numeric>{(value / rearValue).toFixed(2)}</DataTable.Cell>
+                })
+              }
+            </DataTable.Row>
+          })
+        }
+      </DataTable>
+
+
     </View>
   );
 }
@@ -36,5 +61,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  map: {
+    width: 300,
+    height: 200,
   },
 });
