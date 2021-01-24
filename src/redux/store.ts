43864113fc511032250d4f621bc0cc38a57afa53
Axel Forsch <axel.forsch@tutanota.com>
2021-01-24 16:38:28 +0100

    another try

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -4,17 +4,26 @@ import { location } from "./location/reducer";
 import { persistStore, persistReducer } from "redux-persist";
 import AsyncStorage from '@react-native-community/async-storage';
 
+// https://blog.reactnativecoach.com/the-definitive-guide-to-redux-persist-84738167975
+// is this the way?
+
 const persistConfig = {
   key: "root",
   storage: AsyncStorage,
   blacklist: ['location'],
 };
 
+const locationPersistConfig = {
+  key: 'location',
+  storage: AsyncStorage,
+  blacklist: ['location'],
+}
+
 const persistedReducer = persistReducer(
   persistConfig,
   combineReducers({
-    settings,
-    location,
+    settings : settings,
+    location : persistReducer(locationPersistConfig, location),
   })
 );
 
